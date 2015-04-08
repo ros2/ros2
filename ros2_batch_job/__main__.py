67814cb8cff98d6220e10e12e73c41655c34c4ed
William Woodall <william@osrfoundation.org>
2015-04-08 15:57:52 -0700

    trick vcs into outputing color on Windows.

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -102,6 +102,10 @@ def main(sysargv=None):
 
     # Set the TERM env variable to coerce the output of Make to be colored.
     os.environ['TERM'] = os.environ.get('TERM', 'xterm-256color')
+    if args.os == 'windows':
+        # Set the ConEmuANSI env variable to trick some programs (vcs) into
+        # printing ANSI color codes on Windows.
+        os.environ['ConEmuANSI'] = 'ON'
 
     info("Using workspace: @!{0}", fargs=(args.workspace,))
     clean_workspace(args.workspace)

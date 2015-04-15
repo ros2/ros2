d979b0db96d439dedfc1ad4336bb9f4eb52ddd44
William Woodall <william@osrfoundation.org>
2015-04-14 23:29:27 -0700

    move multicast enable into entry point script

--- linux_docker_resources/entry_point.sh
+++ linux_docker_resources/entry_point.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This file fixes the permissions of the home directory so that it matches the host user's ID.
+# It also enables multicast and changes directories before executing the input from docker run.
+
 # Adapted from: http://chapeau.freevariable.com/2014/08/docker-uid.html
 
 export ORIGPASSWD=$(cat /etc/passwd | grep rosbuild)
@@ -17,5 +20,11 @@ sed -i -e "s/rosbuild:x:$ORIG_GID:/rosbuild:x:$GID:/" /etc/group
 
 chown -R ${UID}:${GID} "${ORIG_HOME}"
 echo "done."
+
+echo "Enabling multicast..."
+ifconfig eth0 multicast
+echo "done."
+
+cd /home/rosbuild/ci_scripts
 
 exec sudo -H -u rosbuild -E -- /bin/sh -c "$*"

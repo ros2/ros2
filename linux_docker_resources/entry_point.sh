561defe1355188b6bded3b5076eae24d7d889ed0
Esteve Fernandez <esteve@osrfoundation.org>
2015-05-11 16:08:30 -0700

    Added new connext packages

--- linux_docker_resources/entry_point.sh
+++ linux_docker_resources/entry_point.sh
@@ -28,11 +28,9 @@ echo "done."
 case "${CI_ARGS}" in
   *--connext*)
     echo "Installing Connext..."
-    dpkg -i /tmp/librticonnextdds51_5.1.0-4_amd64.deb
-    dpkg -i /tmp/librticonnextdds51-dev_5.1.0-4_amd64.deb
-    dpkg -i /tmp/rticonnextdds-tools_5.1.0-4_amd64.deb
-    dpkg -i /tmp/librticonnextdds-messaging51_5.1.0-3_amd64.deb
-    dpkg -i /tmp/librticonnextdds-messaging51-dev_5.1.0-3_amd64.deb
+    dpkg -i /tmp/librticonnextdds51_5.1.0.1-1_amd64.deb
+    dpkg -i /tmp/librticonnextdds51-dev_5.1.0.1-1_amd64.deb
+    dpkg -i /tmp/rticonnextdds-tools_5.1.0.1-1_amd64.deb
     echo "done."
     ;;
   *)

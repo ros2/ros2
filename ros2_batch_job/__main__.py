917cfdcdb77c022a755bf1cbb1297f2a7a51dac9
William Woodall <william@osrfoundation.org>
2015-10-27 14:22:41 -0700

    fix reused list

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -237,7 +237,7 @@ def run(args, build_function):
         if args.test_branch is not None:
             # Store current branch as well-known branch name for later rebasing
             info("Attempting to create a well known branch name for all the default branches")
-            vcs_custom_cmd = vcs_cmd
+            vcs_custom_cmd = list(vcs_cmd)
             vcs_custom_cmd += ['custom', '.', '--git', '--args', 'checkout', '-b', '__ci_default']
             ret = job.run(vcs_custom_cmd, exit_on_error=False)
             info("'{0}' returned exit code '{1}'", fargs=(" ".join(vcs_custom_cmd), ret))

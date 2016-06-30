bd01c4fefaff298594152f27170e08f1dd9e770e
dhood <deanna.m.hood@gmail.com>
2016-06-29 17:07:32 -0700

    Error message for if anything unexpected happens while communicating with installer

--- linux_docker_resources/rti_web_binaries_install_script.py
+++ linux_docker_resources/rti_web_binaries_install_script.py
@@ -20,25 +20,31 @@ import pexpect
 def install_connext(installer_path, install_directory):
     child = pexpect.spawn(installer_path + ' --mode text', encoding='utf8')
 
-    keep_agreeing = True
-    while(keep_agreeing):
-        result_index = child.expect([
-            'Installation Directory.*?:',
-            'Press \[Enter\] to continue:',
-            'Do you accept this license\? \[y/n\]: '])
-        if result_index == 0:
-            keep_agreeing = False
-        else:
-            child.sendline('y')
+    try:
+        keep_agreeing = True
+        while(keep_agreeing):
+            result_index = child.expect([
+                'Installation Directory.*?:',
+                'Press \[Enter\] to continue:',
+                'Do you accept this license\? \[y/n\]: '])
+            if result_index == 0:
+                keep_agreeing = False
+            else:
+                child.sendline('y')
 
-    child.sendline(install_directory)
-    child.expect_exact('Do you want to continue? [Y/n]:')
-    child.sendline('y')
-    result_index = child.expect_exact([
-        'Create an RTI Launcher shortcut on the Desktop [y/N]: ', pexpect.EOF])
-    if result_index == 0:
-        child.sendline('n')
-        child.expect(pexpect.EOF)
+        child.sendline(install_directory)
+        child.expect_exact('Do you want to continue? [Y/n]:')
+        child.sendline('y')
+        result_index = child.expect_exact([
+            'Create an RTI Launcher shortcut on the Desktop [y/N]: ', pexpect.EOF])
+        if result_index == 0:
+            child.sendline('n')
+            child.expect(pexpect.EOF)
+
+    except (pexpect.TIMEOUT, pexpect.EOF):
+        raise RuntimeError(
+            'Unexpected output while installing Connext. Last 100 chars received from installer: ' +
+            '\n' + child.before)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

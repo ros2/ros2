b8e76b9e189363fbf58e706ae0145f048d804028
Louis Mayencourt <louis.mayencourt@arm.com>
2017-09-12 15:55:05 +0100

    add cmake toolchainfile for aarch64 cross-compilation
    This toolchainfile can be used with ament to cross-compile
    ROS2 for arm aarch64.
    The CROSS_COMPILE variable should be set to point to a
    valid aarch64 toolchain.
    
    Signed-off-by: Louis Mayencourt <louis.mayencourt@arm.com>

---
+++ toolchainfile.cmake
@@ -0,0 +1,26 @@
+# Copyright (c) 2017, ARM Limited. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_SYSTEM_PROCESSOR aarch64)
+# specify the cross compiler
+SET(CMAKE_C_COMPILER $ENV{CROSS_COMPILE}gcc)
+SET(CMAKE_CXX_COMPILER $ENV{CROSS_COMPILE}g++)
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/install)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
